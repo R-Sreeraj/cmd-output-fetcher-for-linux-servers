25d59b2cc3d4f098b1bd00b0cbb357666d94d169
sree-raj-r <54491972+sree-raj-r@users.noreply.github.com>
2019-08-26 23:59:07 +0530

    Initial commit for connect.py

---
+++ connect.py
@@ -0,0 +1,15 @@
+import paramiko
+p = paramiko.SSHClient()
+server_details = open("server_list.csv","r")
+for each in server_details.readlines():
+        strip_each=each.strip()
+        line=strip_each.split(",")
+        p.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        p.connect("%s"%line[0],port =22, username = "%s"%line[1], password="%s"%line[2])
+        stdin, stdout, stderr = p.exec_command("df -Pkh")
+        output = stdout.readlines()
+        output ="".join(output)
+        result = open("%s.txt"%line[0],"w")
+        result.write(output)
+        result.close()
+server_details.close()
